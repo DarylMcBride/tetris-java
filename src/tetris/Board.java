18ab9ee445a9dea18ce1108bc202e595731f5255
DarylMcBride <daryl332@hotmail.com>
2019-10-06 12:47:24 +0100

    adding textures

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -1,20 +1,34 @@
 package tetris;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 	
+	private BufferedImage tiles;
+	
 	public Board() {
 		
+		
+		try {
+			tiles = ImageIO.read(Board.class.getResource("/tiles.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void paintComponent (Graphics g) {
 		
 		super.paintComponent(g);
 		
-		g.drawRect(100, 100, 50, 50);
+		g.drawImage(tiles, 0,0, null);
+		
+		
 		
 	}
 
